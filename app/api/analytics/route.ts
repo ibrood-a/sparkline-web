6b232edbd32978a90b8ef2375492e9bdd61880c8
jacob <iblooze2@gmail.com>
2024-06-27 02:05:09 -0500

    refactored some things

--- app/api/analytics/route.ts
+++ app/api/analytics/route.ts
@@ -12,19 +12,27 @@ export async function GET(req: Request) {
       return new NextResponse('Unauthorized', { status: 401 })
     }
 
-    const payload = {userId: user?.user.id, iss: process.env.JWT_ISSUER};
-    console.log(payload)
-    const token = jwt.encode(payload, process.env.JWT_SECRET!!, "HS256");
+    const payload = { userId: user.user.id, iss: process.env.JWT_ISSUER };
+    console.log(payload);
+    const token = jwt.encode(payload, process.env.JWT_SECRET!!, 'HS256');
 
-    console.log(payload, token)
+    console.log(payload, token);
     const [videoResponse, channelResponse] = await Promise.all([
-      axios.post(`${process.env.API_URL}/socials/analyze/youtube/post`, token),
-      axios.post(`${process.env.API_URL}/socials/analyze/youtube/page`, token),
+      axios.post(
+        `${process.env.API_URL}/socials/analyze/youtube/post`,
+        { token },
+        { headers: { 'Content-Type': 'application/json' } }
+      ),
+      axios.post(
+        `${process.env.API_URL}/socials/analyze/youtube/page`,
+        { token },
+        { headers: { 'Content-Type': 'application/json' } }
+      ),
     ]);
 
-    return new NextResponse(JSON.stringify({ videoData: videoResponse.data, channelData: channelResponse.data.items[0] }))
+    return new NextResponse(JSON.stringify({ videoData: videoResponse.data, channelData: channelResponse.data.items[0] }));
   } catch (error) {
-    console.error(error)
-    return new NextResponse('Internal Error', { status: 500 })
+    console.error(error);
+    return new NextResponse('Internal Error', { status: 500 });
   }
 }
